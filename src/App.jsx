7d89bf6dcbf3ebf17d1f72895c5ee77a67503c6c
paulolopest <paulotarso-lopes@hotmail.com>
2025-04-03 23:39:51 -0300

    Fixing tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
-import {motion} from 'framer-motion'
 
 const App = () => {
   return (
-    <motion.div className='w-full h-screen flex justify-center items-center bg-red-200 text'>a</motion.div>
+    <div>
+      a
+    </div>
   )
 }
 
