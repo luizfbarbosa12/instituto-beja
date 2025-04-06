51121371217c17eb7c6c27801156aa5384cb1893
paulolopest <paulotarso-lopes@hotmail.com>
2025-04-06 04:46:46 -0300

    Adding CME Section

---
+++ src/data/cmeData.js
@@ -0,0 +1,97 @@
+import CMECard1 from "/public/assets/CMECard1.png";
+import CMECard2 from "/public/assets/CMECard2.png";
+import CMECard3 from "/public/assets/CMECard3.png";
+import MapBiomasLogo from "/public/assets/MapBiomas.png";
+import SerenasLogo from "/public/assets/Serenas_logo.svg";
+import DesenrolaLogo from "/public/assets/DesenrolaLogo.png";
+
+export const AEPIE = [
+  {
+    bgColor: "bg-white",
+    text: "A construção de um ecossistema forte e conectado é essencial para catalisar e fortalecer a agenda dos empreendedores. Para isso, é fundamental compreender os atores envolvidos e as barreiras a serem superadas. Realizar um mapeamento detalhado dos atores estratégicos e identificar os desafios existentes permite destravar essa agenda e impulsionar mudanças sistêmicas de forma colaborativa",
+    img: CMECard1,
+    title: {
+      color: "text-cyan",
+      name: "Ação em Rede",
+    },
+  },
+  {
+    bgColor: "bg-[#FFECFD]",
+    text: "Apoiar a construção de uma infraestrutura ética para o ecossistema brasileiro - envolvendo orquestradores de sistemas, redes, órgãos reguladores, academia e fundações - é essencial para garantir o desenvolvimento responsável e sustentável. Isso inclui a adoção de protocolos que orientem as melhores práticas na tomada de decisões estratégicas sobre tecnologias, prototipagem, financiamento e implementação em larga escala",
+    img: CMECard2,
+    title: {
+      color: "text-[#E679E0]",
+      name: "Ecossistema Local de Impacto com a Tecnologia",
+    },
+  },
+  {
+    bgColor: "bg-[#FFCBC4]",
+    text: "Fomentar uma rede colaborativa global capaz de responder rapidamente aos grandes desafios sociais e ambientais, acelerando soluções por meio do uso de tecnologias responsáveis. Essa rede permitirá a troca de conhecimento, experiências e recursos entre diferentes atores, impulsionando inovações escaláveis e sustentáveis a partir do Sul Global",
+    img: CMECard3,
+    title: {
+      color: "text-hot-coral",
+      name: "Ecossistema Global para Exponenciar Soluções a Partir do Sul",
+    },
+  },
+];
+
+export const VTCME = [
+  {
+    bgColor: "bg-[#AAD5DC] ",
+    title: "NOVOS PARADIGMAS",
+    text: "Impulsionar, colaborativamente novos modelos mentais de empreendedores exponenciais, fortalecendo e apoiando a viabilidade de suas soluções para problemas sistêmicos, de forma a gerarem impacto exponencial e global.",
+  },
+  {
+    bgColor: "bg-[#FFD793]",
+    title: "NETWORK EFFECT",
+    text: "Entender os atores e as barreiras que precisamos mover para catalisar e fortalecer o ecossistema que destrava a agenda destes empreendedores.",
+  },
+  {
+    bgColor: "bg-[#FFCBC4]",
+    title: "ECOSSISTEMA LOCAL DE IMPACTO (INFRA-ESTRUTURA) COM TECNOLOGIA",
+    text: "Criar uma infraestrutura ética que siga protocolos responsáveis para apoiar o ecossistema brasileiro (SOs, network, regulação, academia, fundações) a fazer as melhores escolhas em relação às tecnologias, protótipos, financiamento e implementação em larga escala.",
+  },
+];
+
+export const CMEPartners = [
+  {
+    bgColor: "bg-porcelain",
+    logo: MapBiomasLogo,
+    title: {
+      color: "text-cyan",
+      name: "MapBiomas",
+    },
+    text: [
+      "Revelar as transformações do território brasileiro por meio da ciência, com precisão, agilidade e qualidade, e tornar acessível o conhecimento sobre a cobertura e o uso da terra, para buscar a conservação e o manejo sustentável dos recursos naturais e bloquear financeiramente práticas ilegais de desmatamento e uso ilegal da terra, como forma de combate às mudanças climáticas.",
+      "Desde 2015, o MapBiomas está transformando nossa forma de monitorar a cobertura e o uso da terra no Brasil. Com uma rede de ONGs, universidades e startups de tecnologia, utiliza inteligência artificial e processamento em nuvem para gerar dados abertos e atualizados sobre desmatamento, água e incêndios, desde 1985.",
+      "A plataforma é aberta, escalável e replicável em diferentes países e contextos, permitindo a integração de novos parceiros e a contribuição da comunidade científica. Geramos conhecimento acessível e preciso sobre as transformações do território para impulsionar a conservação, o manejo sustentável dos recursos naturais e o combate às mudanças climáticas.",
+    ],
+  },
+  {
+    bgColor: "bg-[#FFECFD]",
+    logo: SerenasLogo,
+    title: {
+      color: "text-[#E679E0]",
+      name: "Serenas",
+    },
+    text: [
+      "Como a tecnologia pode nos ajudar a construir um Brasil seguro para mulheres e meninas de forma ética, responsável e efetiva?",
+      "Uma jornada que se soma ao trabalho de uma organização que nasceu para que meninas e mulheres possam ser livres, escolher e sonhar sem limites, exercendo seus direitos à dignidade, respeito e igualdade em um mundo sem discriminação.",
+      "A Serenas é uma organização sem fins lucrativos que, desde 2021, apoia governos na construção e implementação de políticas educacionais de prevenção de violências contra meninas e mulheres, além da qualificação dos serviços públicos para um atendimento humanizado, intersetorial e efetivo a sobreviventes de violência sexual e doméstica.",
+      "Como a Serenas, acreditamos que a transformação exige um trabalho profundo de mudança dos valores e normas sociais que sustentam as violências e isso se dá através da educação das novas gerações e daqueles que atuam no atendimento de vítimas, garantindo um atendimento adequado e respeitoso.",
+    ],
+  },
+  {
+    bgColor: "bg-[#FFCBC4]",
+    logo: DesenrolaLogo,
+    title: {
+      color: "text-hot-coral",
+      name: "Desenrola e não me enrola",
+    },
+    text: [
+      "Desenvolve jornalismo comunitário e educação antirracista e midiática em favelas e periferias urbanas, para construir ecossistemas informacionais para combater as desigualdades digitais e informacionais. (nomeação feita pelo Instituto Beja)",
+      "Criado em 2013 por moradores das periferias, seu trabalho descentraliza a produção de conhecimento e impulsiona o engajamento cívico, criando soluções inovadoras para enfrentar a desinformação, segregação digital e exclusão midiática.",
+      "Ao desenvolver tecnologias escaláveis que possibilitam a educação midiática, oportunidades de emprego e acesso a informações de melhor qualidade, o Desenrola exerce um papel vital na promoção de ecossistemas informacionais inclusivos que reduzem desigualdades e fortalecem a democracia.",
+    ],
+  },
+];
