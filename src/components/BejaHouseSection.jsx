56f9d06d1e551c830119077536b3975a5eb045a8
paulolopest <paulotarso-lopes@hotmail.com>
2025-04-13 22:07:33 -0300

    Changing wrapper logic and updating structure

--- src/components/BejaHouseSection.jsx
+++ src/components/BejaHouseSection.jsx
@@ -3,72 +3,75 @@ import EspacoBeja2 from "../../public/assets/EspacoBeja2.png";
 import EspacoBeja3 from "../../public/assets/EspacoBeja3.png";
 import ImageSlider from "./ImageSlider";
 import TextBlock from "./TextBlock";
+import Wrapper from "./Wrapper";
 
 const BejaHouseSection = () => {
   return (
     <div className='flex flex-col relative rounded-[70px]'>
-      <div className='relative -left-29 max-w-none w-screen'>
+      <div className='relative w-full left-0'>
         <img src={EspacoBeja} alt='Espaço Beja image' className='' />
       </div>
 
-      <div className='flex flex-col gap-[3.7rem] py-[6.5rem]'>
-        <h1 className='editorial text-8xl'>Casa Beja</h1>
+      <Wrapper>
+        <div className='flex flex-col gap-[3.7rem] py-[6.5rem]'>
+          <h1 className='editorial text-8xl'>Casa Beja</h1>
 
-        <TextBlock
-          firstBlock={[
-            {
-              key: "first-0",
-              content: (
-                <>
-                  <p>
-                    Com alegria apresentamos a <strong>Casa Beja</strong>, a
-                    nova morada dos nossos valores. Inaugurada em novembro de
-                    2024, na cidade de São Paulo, a <strong>Casa Beja</strong>{" "}
-                    vai muito além de um simples escritório, assim como a sede
-                    no Rio de Janeiro, é um ambiente vivo onde ideias se
-                    encontram, parcerias florescem e soluções sistêmicas ganham
-                    forma. É aqui que buscamos conectar indivíduos e
-                    comunidades, semear a cultura da filantropia e inspirar
-                    ações coletivas que constroem um futuro mais solidário e
-                    sustentável.
-                  </p>
-                </>
-              ),
-            },
-          ]}
-          secondBlock={[
-            {
-              key: "second-0",
-              content: (
-                <>
-                  <p>
-                    Com grande honra, incubamos o hub Brasil do{" "}
-                    <strong>Centro para Mudanças Exponenciais</strong>,
-                    reafirmando nosso compromisso com a inovação e com o impacto
-                    social. Este espaço é ponto de convergência entre sociedade
-                    civil, setor privado e governo, unindo forças para criar
-                    colaborações que impulsionam soluções e transformam
-                    realidades sociais de maneira eficaz e inclusiva.
-                  </p>
-                  <p>
-                    <strong>A Casa Beja</strong> simboliza{" "}
-                    <strong>movimento, relações e articulação -</strong>{" "}
-                    refletindo a essência do nosso logo, que ilustra a dinâmica
-                    constante entre pilares e a busca por equilíbrio nas
-                    relações humanas.
-                  </p>
-                </>
-              ),
-            },
-          ]}
-        />
-      </div>
+          <TextBlock
+            firstBlock={[
+              {
+                key: "first-0",
+                content: (
+                  <>
+                    <p>
+                      Com alegria apresentamos a <strong>Casa Beja</strong>, a
+                      nova morada dos nossos valores. Inaugurada em novembro de
+                      2024, na cidade de São Paulo, a <strong>Casa Beja</strong>{" "}
+                      vai muito além de um simples escritório, assim como a sede
+                      no Rio de Janeiro, é um ambiente vivo onde ideias se
+                      encontram, parcerias florescem e soluções sistêmicas
+                      ganham forma. É aqui que buscamos conectar indivíduos e
+                      comunidades, semear a cultura da filantropia e inspirar
+                      ações coletivas que constroem um futuro mais solidário e
+                      sustentável.
+                    </p>
+                  </>
+                ),
+              },
+            ]}
+            secondBlock={[
+              {
+                key: "second-0",
+                content: (
+                  <>
+                    <p>
+                      Com grande honra, incubamos o hub Brasil do{" "}
+                      <strong>Centro para Mudanças Exponenciais</strong>,
+                      reafirmando nosso compromisso com a inovação e com o
+                      impacto social. Este espaço é ponto de convergência entre
+                      sociedade civil, setor privado e governo, unindo forças
+                      para criar colaborações que impulsionam soluções e
+                      transformam realidades sociais de maneira eficaz e
+                      inclusiva.
+                    </p>
+                    <p>
+                      <strong>A Casa Beja</strong> simboliza{" "}
+                      <strong>movimento, relações e articulação -</strong>{" "}
+                      refletindo a essência do nosso logo, que ilustra a
+                      dinâmica constante entre pilares e a busca por equilíbrio
+                      nas relações humanas.
+                    </p>
+                  </>
+                ),
+              },
+            ]}
+          />
+        </div>
+      </Wrapper>
 
-      <div className='flex-center relative flex-col w-screen max-w-none -left-29 bg-retro-ochre gap-24 py-50'>
+      <div className='flex-center relative flex-col w-full bg-retro-ochre gap-24 py-50'>
         <h1 className='text-5xl text-porcelain editorial'>
           Bem-vindo(a) à Casa Beja!
         </h1>
-
         <ImageSlider
           arr={[EspacoBeja, EspacoBeja2, EspacoBeja3]}
           translateYMiddleImg={"80"}
--- src/components/StrategicPillarSection.jsx
+++ src/components/StrategicPillarSection.jsx
@@ -1,30 +1,32 @@
 import { SPCardData } from "../data/StrategicPillarData";
 import Pillars from "../../public/assets/PilaresEstrategicos.png";
 import ScrollTriggerList from "./ScrollTriggerCard/ScrollTriggerList";
+import Wrapper from "./Wrapper";
 
 const StrategicPillarSection = () => {
   return (
-    <div className='flex flex-col gap-38 w-full pt-80'>
-      <h1 className='editorial text-8xl w-[60rem] leading-32 pl-32'>
-        Pilares Estratégicos
-      </h1>
+    <Wrapper className={"flex flex-col gap-30"}>
+      <div className='flex flex-col gap-38 w-full pt-80'>
+        <h1 className='editorial text-8xl w-[60rem] leading-32 '>
+          Pilares Estratégicos
+        </h1>
 
-      <div className='flex items-start justify-end w-full gap-[9rem] px-32'>
-        <img src={Pillars} alt='Pilares Estratégicos' />
-        <p className='w-[29.5rem] text-base'>
-          Nossos três pilares estratégicos – advocacy, conhecimento e
-          laboratórios – são as forças motrizes que orientaram nossas ações ao
-          longo de 2024. Funcionaram como lentes de estudos, práticas e
-          experimentações, sustentando as reflexões do Beja sobre os modos de
-          fazer diferente. Sem restringir abordagens, esses pilares condensam
-          instrumentos para nossos exercícios de ação e inovação.
-        </p>
+        <div className='flex items-start justify-end w-full gap-[9rem]'>
+          <img src={Pillars} alt='Pilares Estratégicos' />
+          <p className='w-[29.5rem] text-base'>
+            Nossos três pilares estratégicos – advocacy, conhecimento e
+            laboratórios – são as forças motrizes que orientaram nossas ações ao
+            longo de 2024. Funcionaram como lentes de estudos, práticas e
+            experimentações, sustentando as reflexões do Beja sobre os modos de
+            fazer diferente. Sem restringir abordagens, esses pilares condensam
+            instrumentos para nossos exercícios de ação e inovação.
+          </p>
+        </div>
       </div>
-
       <div className='flex flex-col w-full  gap-10'>
         <ScrollTriggerList arr={SPCardData} />
       </div>
-    </div>
+    </Wrapper>
   );
 };
 
