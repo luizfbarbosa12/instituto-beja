b0b93ca8c066d863eef45c4543a01c955f751275
paulolopest <paulotarso-lopes@hotmail.com>
2025-04-30 13:29:59 -0300

    Advocacy responissivines

---
+++ src/components/AdvocacySection/PartnerSection.jsx
@@ -0,0 +1,31 @@
+import AdvocacyCard from "./AdvocacyCard";
+
+const PartnerSection = ({
+  children,
+  partnerName,
+  title,
+  mainImg,
+  cardsImg,
+  subtitle,
+}) => {
+  return (
+    <div className='flex flex-col gap-16 w-full px-32 items-end max-[1280px]:px-16 max-[880px]:px-8 max-[650px]:px-5 max-sm:gap-8'>
+      <AdvocacyCard
+        title={title}
+        mainImg={mainImg}
+        img={cardsImg}
+        subtitle={subtitle}
+      />
+
+      <div className='flex flex-col gap-18 w-[66rem] max-[1325px]:w-full max-[650px]:gap-4'>
+        <h1 className='editorial text-6xl flex-wrap w-[70%] leading-20 max-[1100px]:w-full max-md:text-5xl max-md:leading-15 max-sm:text-2xl max-sm:leading-10'>
+          {partnerName}
+        </h1>
+
+        {children}
+      </div>
+    </div>
+  );
+};
+
+export default PartnerSection;
--- src/components/TextBlock.tsx
+++ src/components/TextBlock.tsx
@@ -4,30 +4,29 @@ type TextBlockProps = {
   firstBlock?: { key: string; content: JSX.Element }[];
   secondBlock?: { key: string; content: JSX.Element }[];
 };
+
 const TextBlock = ({ firstBlock, secondBlock }: TextBlockProps) => {
   return (
-    <div className='flex justify-end'>
-      <div className='flex gap-12 max-w-264 max-xl:flex-col text-base'>
-        {firstBlock && (
-          <>
-            {firstBlock.map(({ key, content }) => (
-              <div className='flex flex-col w-full gap-8' key={key}>
-                {content}
-              </div>
-            ))}
-          </>
-        )}
+    <div className='flex justify-end gap-12 max-w-264 max-[650px]:flex-col max-[650px]:gap-8 max-sm:gap-4'>
+      {firstBlock && (
+        <div className='w-1/2 text-base max-[650px]:w-full max-sm:text-[12px]'>
+          {firstBlock.map(({ key, content }) => (
+            <div className='flex flex-col w-full gap-8' key={key}>
+              {content}
+            </div>
+          ))}
+        </div>
+      )}
 
-        {secondBlock && (
-          <>
-            {secondBlock.map(({ key, content }) => (
-              <div className='flex flex-col w-full gap-8' key={key}>
-                {content}
-              </div>
-            ))}
-          </>
-        )}
-      </div>
+      {secondBlock && (
+        <div className='w-1/2 text-base max-[650px]:w-full max-sm:text-[12px]'>
+          {secondBlock.map(({ key, content }) => (
+            <div className='flex flex-col w-full gap-8' key={key}>
+              {content}
+            </div>
+          ))}
+        </div>
+      )}
     </div>
   );
 };
