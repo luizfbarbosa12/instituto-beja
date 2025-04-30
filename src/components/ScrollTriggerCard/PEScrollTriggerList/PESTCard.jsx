7f066f753c6abc9e80b28368393a805388612547
Rellyson <rellysondouglas2015@gmail.com>
2025-04-30 19:49:09 -0300

    feat: add Strategic pillar section mobile layout

--- src/components/ScrollTriggerCard/PEScrollTriggerList/PESTCard.jsx
+++ src/components/ScrollTriggerCard/PEScrollTriggerList/PESTCard.jsx
@@ -17,11 +17,11 @@ const PESTCard = ({
     <motion.div
       layout
       ref={container}
-      className='h-screen flex-center py-20 sticky top-0 w-full'
+      className='flex-center not-first:pt-10 not-first:tablet:pt-20 not-last:tablet:pb-20 not-last:pb-10 sticky top-0 w-full'
     >
       <motion.div
         layout
-        className={`flex flex-col relative w-full h-190 rounded-[4rem] p-22.5 gap-6.5 ${cssOverride}`}
+        className={`min-h-screen flex flex-col relative w-full rounded-[4rem] overflow-hidden p-22.5 gap-3.5 tablet:gap-6.5 ${cssOverride}`}
         style={{
           top: `calc(-10% + ${idx * 4}rem)`,
           scale: cardScale,
@@ -29,18 +29,23 @@ const PESTCard = ({
           color: data.textColor,
         }}
       >
-        <h1 className='editorial text-6xl'>{data.title}</h1>
+        <h1 className='editorial text-[28px] not-tablet:leading-[65px] text-6xl'>
+          {data.title}
+        </h1>
 
-        <motion.div layout className='flex w-full gap-8'>
-          <p className='editorial text-3xl w-full leading-12'>
+        <motion.div layout className='flex not-tablet:flex-col w-full gap-8'>
+          <p className='editorial text-lg tablet:text-3xl w-full tablet:leading-12'>
             {data.subtitle}
           </p>
-          <motion.p layout className='text-base w-full leading-6 z-10'>
+          <motion.p
+            layout
+            className='tablet:text-base text-xs leading-relaxed w-full tablet:leading-6 z-10'
+          >
             {data.text}
           </motion.p>
         </motion.div>
 
-        <div className='flex flex-col gap-7 w-full editorial'>
+        <div className='flex flex-col gap-7 w-full editorial not-tablet:pb-80'>
           <div
             className='h-0.5 w-1/3 z-10'
             style={{ backgroundColor: data.lineColor }}
@@ -51,7 +56,18 @@ const PESTCard = ({
             style={{ "--tw-marker-color": data.lineColor }}
           >
             {data.list.map((item, index) => (
-              <li key={index}>{item}</li>
+              <div key={index} className='flex gap-4 items-center'>
+                <span
+                  className='size-2 tablet:size-5 rounded-full shrink-0'
+                  style={{ backgroundColor: data.lineColor }}
+                />
+                <li
+                  className='list-none text-xs leading-relaxed tablet:text-3xl'
+                  key={index}
+                >
+                  {item}
+                </li>
+              </div>
             ))}
           </ul>
         </div>
@@ -59,7 +75,7 @@ const PESTCard = ({
         <img
           src={data.img}
           alt='Imagem de uma mão'
-          className='absolute bottom-0 right-40  w-fit h-[24rem]'
+          className='absolute object-contain bottom-0 right-0 h-80 tablet:w-2/5 tablet:h-auto tablet:max-h-1/2'
         />
       </motion.div>
     </motion.div>
