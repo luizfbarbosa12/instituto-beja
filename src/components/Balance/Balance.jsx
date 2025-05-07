7acad9ba7c47f4180073ae7ec1de62de7527ece2
luizfbarbosa12 <l.nandoferbarbosa@gmail.com>
2025-05-07 20:31:52 -0300

    balanço mobile ok

--- src/components/Balance/Balance.jsx
+++ src/components/Balance/Balance.jsx
@@ -1,60 +1,67 @@
 const BalancoTable = ({ data }) => {
   return (
-    <div className='py-12 px-24'>
-      <div className='flex justify-between items-end mb-8'>
-        <h1 className='text-6xl'>Balanço</h1>
+    <div className="py-12 px-4 sm:px-24">
+      <div className="flex flex-col sm:flex-row justify-between items-end mb-8">
+        <h1 className="text-3xl sm:text-6xl">Balanço</h1>
 
-        <div className='w-[650px]'>
-          <table className='w-full border-collapse'>
-            <thead>
-              <tr className='bg-bourdeaux text-white '>
-                <th className='py-3 px-4 text-left text-lg'>PROJETO</th>
-                <th className='py-3 px-4 text-right'>
-                  TOTAL DO
-                  <br />
-                  APORTE EM R$
-                </th>
-                <th className='py-3 px-4 text-right'>
-                  TOTAL 2024
-                  <br />
-                  EM R$
-                </th>
-                <th className='py-3 px-4 text-center'>INÍCIO</th>
-                <th className='py-3 px-4 text-center'>CONCLUSÃO</th>
-              </tr>
-            </thead>
-          </table>
+        <div className="w-full sm:w-[650px] mt-4 sm:mt-0">
+          <div className="overflow-x-auto">
+            <table className="w-full border-collapse">
+              <thead>
+                <tr className="bg-bourdeaux text-white ">
+                  <th className="py-2 px-2 sm:py-3 sm:px-4 text-left text-xs sm:text-lg">
+                    PROJETO
+                  </th>
+                  <th className="py-2 px-2 sm:py-3 sm:px-4 text-right text-xs sm:text-base">
+                    TOTAL DO<br/>APORTE EM R$
+                  </th>
+                  <th className="py-2 px-2 sm:py-3 sm:px-4 text-right text-xs sm:text-base">
+                    TOTAL 2024<br/>EM R$
+                  </th>
+                  <th className="py-2 px-2 sm:py-3 sm:px-4 text-center text-xs sm:text-base">
+                    INÍCIO
+                  </th>
+                  <th className="py-2 px-2 sm:py-3 sm:px-4 text-center text-xs sm:text-base">
+                    CONCLUSÃO
+                  </th>
+                </tr>
+              </thead>
+            </table>
+          </div>
         </div>
       </div>
 
       {data.tableData.map((section, sectionIndex) => (
-        <div key={sectionIndex} className={"mt-8 "}>
+        <div key={sectionIndex} className="mt-4 sm:mt-8">
           {section.category && (
-            <div className='bg-retro-ochre rounded-3xl py-2 px-4 font-bold'>
+            <div className="bg-retro-ochre rounded-3xl py-1 px-2 sm:py-2 sm:px-4 font-bold text-xs sm:text-sm">
               {section.category}
             </div>
           )}
 
-          <div className='overflow-x-auto'>
-            <table className='w-full border-collapse'>
+          <div className="overflow-x-auto mt-2 sm:mt-4">
+            <table className="w-full border-collapse">
               <tbody>
                 {section.rows.map((item, index) => (
-                  <tr
-                    key={index}
-                    className='border-b border-dashed border-rose'
-                  >
-                    <td className='py-4 px-2 w-1/4'>{item.project}</td>
-                    <td className='py-4 px-2 text-center w-1/6'>{item.type}</td>
-                    <td className='py-4 px-2 text-right w-1/6'>
+                  <tr key={index} className="border-b border-dashed border-rose">
+                    <td className="py-2 px-1 sm:py-4 sm:px-2 w-1/4 text-xs sm:text-sm">
+                      {item.project}
+                    </td>
+                    <td className="py-2 px-1 sm:py-4 sm:px-2 text-center w-1/6 text-xs sm:text-sm">
+                      {item.type}
+                    </td>
+                    <td className="py-2 px-1 sm:py-4 sm:px-2 text-right w-1/6 text-xs sm:text-sm">
                       {item.totalAmount}
                     </td>
-                    <td className='py-4 px-2 text-right w-1/6'>
+                    <td className="py-2 px-1 sm:py-4 sm:px-2 text-right w-1/6 text-xs sm:text-sm">
                       {item.amount2024}
                     </td>
-                    <td className='py-4 px-2 text-center w-1/12'>
+                    <td className="py-2 px-1 sm:py-4 sm:px-2 text-center w-1/12 text-xs sm:text-sm">
                       {item.start}
                     </td>
-                    <td className='py-4 px-2 text-center w-1/12'>{item.end}</td>
+                    <td className="py-2 px-1 sm:py-4 sm:px-2 text-center w-1/12 text-xs sm:text-sm">
+                      {item.end}
+                    </td>
                   </tr>
                 ))}
               </tbody>
