4efc9ccaa9663a29b9b31be0581b8a303831ec40
rafabez <rafabeznos@gmail.com>
2025-05-18 14:16:09 +0300

    fix:layout
    Alinhamento dos links para site e download na home

--- src/components/FirstSection/VisitInstitutoBejaLink.tsx
+++ src/components/FirstSection/VisitInstitutoBejaLink.tsx
@@ -14,7 +14,7 @@ export default function VisitInstitutoBejaLink() {
       <span className="h-8 w-8 flex items-center justify-center">
         <Globe className="size-7 tablet:size-8 text-porcelain group-hover:text-rose transition-colors duration-200" strokeWidth={2.1} />
       </span>
-      <span className="text-base leading-[100%] text-porcelain font-bold whitespace-nowrap group-hover:text-rose transition-colors duration-200">
+      <span className="text-base leading-[100%] text-porcelain font-bold whitespace-nowrap group-hover:text-rose transition-colors duration-200 pl-2">
         {t("firstSection.visitInstituto")}
       </span>
     </a>
